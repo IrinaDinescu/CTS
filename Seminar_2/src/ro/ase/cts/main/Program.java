2197ac04b6df36e0180496b5fec4c51309047661
Irina Dinescu <irina.dinescu27@gmail.com>
2021-03-11 08:19:36 +0200

    seminar3 first

--- Seminar_2/src/ro/ase/cts/main/Program.java
+++ Seminar_2/src/ro/ase/cts/main/Program.java
@@ -1,26 +1,26 @@
 package ro.ase.cts.main;
-import ro.ase.cts.classes.*;
-import ro.ase.cts.readers.*;
+
+import ro.ase.cts.classes.Aplicant;
+import ro.ase.cts.readers.AngajatReader;
+import ro.ase.cts.readers.AplicantReader;
+import ro.ase.cts.readers.StudentReader;
 
 import java.io.FileNotFoundException;
 import java.util.List;
 
-
 public class Program {
 	
-	public static List<Aplicant> citesteAplicanti( AplicantReader reader) throws FileNotFoundException, NumberFormatException{
-		
+	public static List<Aplicant> citesteAplicanti(AplicantReader reader) throws NumberFormatException, FileNotFoundException
+	{
 		return reader.citesteAplicanti();
 	}
-	
 
 	public static void main(String[] args) {
-			
-		
 		List<Aplicant> listaAplicanti;
+		AplicantReader aplicantReader = new StudentReader("studenti.txt");
 		try {
 			listaAplicanti = citesteAplicanti(new AngajatReader("angajati.txt"));
-			for(Aplicant angajat:listaAplicanti)
+			for(Aplicant angajat : listaAplicanti)
 				System.out.println(angajat.toString());
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -29,4 +29,3 @@ public class Program {
 	}
 
 }
-
--- Seminar_2/src/ro/ase/cts/readers/AplicantReader.java
+++ Seminar_2/src/ro/ase/cts/readers/AplicantReader.java
@@ -2,6 +2,8 @@ package ro.ase.cts.readers;
 
 import java.io.FileNotFoundException;
 import java.util.List;
+import java.util.Scanner;
+
 import ro.ase.cts.classes.*;
 
 public abstract class AplicantReader {
@@ -19,5 +21,22 @@ public abstract class AplicantReader {
 		return fileName;
 	}
 
+	public void citesteAplicant(Scanner input, Aplicant aplicant) {
+		String nume = input.next();
+		String prenume = input.next();
+		int varsta = input.nextInt();
+		int punctaj = input.nextInt();
+		int nr = input.nextInt();
+		String[] vect = new String[5];
+		for (int i = 0; i < nr; i++)
+			vect[i] = input.next();
+		
+		
+		aplicant.setNume(nume);
+		aplicant.setPrenume(prenume);
+		aplicant.setVarsta(varsta);
+		aplicant.setPunctaj(punctaj);
 	
+		
+	}
 }
